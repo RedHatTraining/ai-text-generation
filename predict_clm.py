3b2518d08146f7fe047eed917d12f5db44c7b1af
Jaime Ramirez <jaime.ramirez@redhat.com>
2024-12-20 18:09:29 +0100

    Add uv and update transformers

--- predict_clm.py
+++ predict_clm.py
@@ -1,17 +1,29 @@
 from transformers import pipeline, set_seed
+from transformers import logging as transformers_logging
 
-TEXT = "=== Identifying the Need"
-wordcount = len(TEXT.split())
+# Suppress warning messages
+transformers_logging.set_verbosity_error()
+
+prompt = "OpenShift AI is"
+max_input_chars = 1000
+wordcount = len(prompt.split())
+
 
 # generator = pipeline('text-generation', model='gpt2')
-generator = pipeline('text-generation', model='.output/')
+generator = pipeline("text-generation", model=".model/")
 set_seed(42)
-predictions = generator("Correct any reported",
-                        max_length=wordcount + 3, num_return_sequences=5)
 
-for p in predictions:
-    print()
-    print("-" * 100)
-    print(p["generated_text"])
-    print("-" * 100)
-    print()
+
+def generate(text: str):
+    predictions = generator(text, max_new_tokens=10, num_return_sequences=1)
+    return predictions[0]["generated_text"]
+
+
+print(prompt, end=None)
+
+while True:
+    input = prompt[-max_input_chars:]
+    generated = generate(input)
+
+    print(generated.replace(input, "").strip(), end=" ", flush=True)
+    prompt = generated
